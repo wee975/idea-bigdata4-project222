0743120916c27c468f2153268dcd24a781e7be16
zhangchen <windyz@163.com>
2021-07-08 14:35:35 +0800

    3333333

--- src/main/java/com/atguigu/jianfa1205/DugujiujianApp.java
+++ src/main/java/com/atguigu/jianfa1205/DugujiujianApp.java
@@ -2,7 +2,7 @@ package com.atguigu.jianfa1205;
 
 public class DugujiujianApp {
     public static void main(String[] args) {
-        System.out.println("hello dugujiujian......9999999966666....");
+        System.out.println("hello dugujiujianaaaaaaaaaaaa......9999999966666....");
 
     }
 }
